1134200281a074d21cba54c090f21f62f0f8040b
Marcus <marcus.tuenmuk20@ycdsbk12.ca>
2019-10-16 13:48:43 -0400

    added the API

--- main.py
+++ main.py
@@ -20,3 +20,23 @@ class Product():
         self.barcode = barcode
         pass
 
+    def on_trolly(self):
+        """Returns the trolly number
+        """
+
+    def off_trolly(self, trolly: int):
+        """Deletes the trolly attribute
+        Args:
+            trolly: the trolly number
+        Return:
+            deletes the trolly number and returns a bin number
+        """
+
+    def in_bin(self, shelf: int, compartment: int):
+        """Adds the bin into a shelf and compartment
+        Args:
+            shelf: where the shelf is
+            compartment: where the compartment is 
+        Return:
+            the attribute of the new object
+        """
