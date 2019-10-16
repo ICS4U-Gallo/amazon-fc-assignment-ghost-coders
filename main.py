6e5f739cce6a395d028c98c3734bbfcaa0100d6d
Marcus <marcus.tuenmuk20@ycdsbk12.ca>
2019-10-16 13:31:49 -0400

    added basic API thing

---
+++ main.py
@@ -0,0 +1,22 @@
+"""
+Product
+====
+Barcode -> object
+Trolly number 
+Bin number
+Shelf number
+Compartment number
+"""
+
+class Product():
+    """
+    This the class for the products coming in 
+    """
+
+    def __init__(self):
+        """
+        This is the inti function for all the main parts of the code
+        """
+        pass
+
+    
