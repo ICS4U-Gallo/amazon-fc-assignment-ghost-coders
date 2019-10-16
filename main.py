117a74ab6a3c4222e93c883e26efbe14a6a565d9
Marcus <marcus.tuenmuk20@ycdsbk12.ca>
2019-10-16 13:33:42 -0400

    added barcode

--- main.py
+++ main.py
@@ -13,10 +13,10 @@ class Product():
     This the class for the products coming in 
     """
 
-    def __init__(self):
+    def __init__(self, barcode: int):
         """
         This is the inti function for all the main parts of the code
         """
+        self.barcode = barcode
         pass
 
-    
