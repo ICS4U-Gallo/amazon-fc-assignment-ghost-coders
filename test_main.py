8fae525826147dbe98b3e398028c1c0630de8471
hongs1234 <47427352+hongs1234@users.noreply.github.com>
2019-10-18 00:46:07 -0400

    Update test_main.py

--- test_main.py
+++ test_main.py
@@ -1,3 +1,12 @@
 from main import *
+import unittest
 
-# def test_
+class test_Product(unittest.TestCase):
+  def setup(self):
+    self.product = Product()
+
+  def test_on_trolly(self):
+#     self.assertEqual()
+
+  def test_in_bin(self):
+    pass
