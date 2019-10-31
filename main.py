6eed95681fcfffcaaf6b320c909ed9a622c4ffc2
Johnson321321 <ziang.wang20@ycdsbk12.ca>
2019-11-01 02:33:31 +0800

    Merge

--- main.py
+++ main.py
@@ -46,17 +46,6 @@ class Trolly:
                 Trolly.trolly_list.remove(code)
     
 
-<<<<<<< HEAD
-
-# class Shelf:
-#     """Shelf Class
-#     Holds the product in a specific shelf and compartment location (number)
-#     """
-#     def __init__(self, shelf_num: str, compartment_num: int):
-#         self.shelf_num = shelf_num
-#         self.compartment_num = compartment_num
-
-=======
         storage.append(product.barcode)
 
         with open("trolly.json", "w") as f:
@@ -80,7 +69,6 @@ class Shelf:
         # self.shelf_num = shelf_num
         # self.compartment_num = compartment_num
         pass
->>>>>>> 08148575024c1d804af10506507dbf9dc03cdaea
 
 #     def add(self, shelf_num: str, compartment_num: int, item: Product):
 #         with open("Compartment.json", 'r') as f:
@@ -110,76 +98,6 @@ class Bin:
     def __init__(self):
         pass
 
-<<<<<<< HEAD
-    def add(self, bin_num: int, item: Product):
-        if bin_num not in Bin.bin_dict.keys():
-            Bin.bin_dict[bin_num] = item
-        else:
-            Bin.bin_dict[bin_num] = item
-
-    def remove(self, item: Product):
-        for value in Bin.bin_dict.values():
-            if value == item:
-                value = ""
-
-
-class Packaging:
-    """Packaging Class
-    Prepares the products (the order) for shipment to customer
-    Attributes:
-        box_type = small, medium, large, fragile
-        address = where product is sent to
-        truck = transportation, license plate
-    """
-
-    def __init__(self, box_type: str, address: str, truck: str):
-        self.box_type = box_type
-        self.address = address
-        self.truck = truck
-    
-    def __str__(self):
-        return "The product(s) has been shipped"
-
-    
-def main():
-    shirt = Product(12345)
-    pants = Product(98765)
-
-    trolly = Trolly()
-    package_1 = Product("AMZ001")
-    package_2 = Product("AMZ002")
-    
-    trolly.add_product(package_1)
-    trolly.add_product(package_2)
-    print(Trolly.trolly_list)
-    trolly.delate_product(package_1)
-    print(Trolly.trolly_list)
-    
-        for key, value in storage.items():
-            for key, value in value.items():
-                for barcode in value:
-                    if item.barcode in value:
-                        value.remove(item.barcode)
-
-
-class Bin:
-    """Bin Class
-    Collects the products ordered and carries it to the packaging station
-    """
-    def __init__(self, bin_num: int):
-        self.bin_num = bin_num
-
-    def add(self, bin_num: int, item: Product):
-        if bin_num not in Bin.bin_dict.keys():
-            Bin.bin_dict[bin_num] = item
-        else:
-            Bin.bin_dict[bin_num] = item
-
-    def remove(self, item: Product):
-        for value in Bin.bin_dict.values():
-            if value == item:
-                value = ""
-=======
     def add(self, product: Product):
         with open("bin.json", "r") as f:
             storage = json.load(f)
@@ -197,7 +115,6 @@ class Bin:
 
         with open("bin.json", "w") as f:
             json.dump(storage, f)
->>>>>>> 08148575024c1d804af10506507dbf9dc03cdaea
 
 
 class Packaging:
@@ -236,8 +153,6 @@ def main():
     print(shirt)
     print(pants)
     print(trolly)
-
->>>>>>> Stashed changes
 
     
 if __name__ == "__main__":
